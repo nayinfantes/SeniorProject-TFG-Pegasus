6283eddd264761c9833f5d549cfa6261ae20a69d
Nayra Infantes <nayra.infantes.tardon@gmail.com>
2023-05-03 12:30:17 +0200

    Senior Project 1st week

---
+++ Pegasus/app/src/main/java/com/example/pegasus/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.pegasus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
